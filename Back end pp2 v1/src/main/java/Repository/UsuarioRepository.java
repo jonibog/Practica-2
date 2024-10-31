2a6bff8b6286a12b3803eed68559daec2a754b96
jonibog <135062183+jonibog@users.noreply.github.com>
2024-10-31 17:13:02 -0300

    Delete Back end pp2 v1 directory

--- Back end pp2 v1/src/main/java/Repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package Repository;
-
-import Entidad.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
