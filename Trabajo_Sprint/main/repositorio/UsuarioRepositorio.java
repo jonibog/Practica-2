b412ce62e2e98de5e4510b774ef7d8e8c891d79c
jonibog <135062183+jonibog@users.noreply.github.com>
2025-03-13 15:45:32 -0300

    Delete Trabajo_Sprint/main directory

--- Trabajo_Sprint/main/repositorio/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package main.repositorio;
-
-import main.modelo.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-}
