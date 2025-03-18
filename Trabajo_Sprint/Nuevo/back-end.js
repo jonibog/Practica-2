8417cd0931eee7e65e1ff019ab4266d18d76ed09
jonibog <135062183+jonibog@users.noreply.github.com>
2025-03-18 11:19:02 -0300

    Delete Trabajo_Sprint directory

--- Trabajo_Sprint/Nuevo/back-end.js
+++
@@ -1,9 +0,0 @@
-// Actualiza la URL para que apunte al servidor del back-end
-fetch('http://localhost:8080/api/productos')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
